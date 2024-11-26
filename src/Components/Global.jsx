3c6fb565561bf85932621e87029e818488479097
Gilles Gonzalez Oropeza <gilles.gonzalezoropeza@gmail.com>
2024-11-26 15:36:33 +0100

    fix sprite going out of the classroom, refactor functions from Student to Agent

--- src/Components/Global.jsx
+++ src/Components/Global.jsx
@@ -2,15 +2,15 @@ export const WindowWidth = window.innerWidth;
 export const WindowHeight = window.innerHeight;
 export const TopLeft = {
     x:0.009191176 * WindowWidth,
-    y:0.727047146 * WindowHeight
+    y:0.700047146 * WindowHeight
 };
 export const DownVector = {
-    x:0.404411765 * WindowWidth - TopLeft.x,
-    y:0.990123457 * WindowHeight - TopLeft.y
+    x:(0.404411765 - 0.009191176) * WindowWidth,
+    y:(0.990123457 - 0.727047146) * WindowHeight
 };
 export const RightVector = {
-    x:0.59375 * WindowWidth - TopLeft.x,
-    y:1/3 * WindowHeight - TopLeft.y
+    x:(0.59375 - 0.009191176) * WindowWidth,
+    y:(1/3 - 0.727047146) * WindowHeight
 };
 export const DownRightVector = {
     x:RightVector.x,
