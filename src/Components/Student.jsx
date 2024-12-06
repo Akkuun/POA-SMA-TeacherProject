f68a5ae5d48f4ae7557a1bdf622e48a48697be6b
Gilles Gonzalez Oropeza <gilles.gonzalezoropeza@gmail.com>
2024-12-06 12:02:59 +0100

    strategie en commentaire

--- src/Components/Student.jsx
+++ src/Components/Student.jsx
@@ -1,12 +1,42 @@
 import { Agent } from './Agent';
 
+
+export const StudentState = {
+    Idle,
+    MovingToCandy,
+    MovingToDesk,
+}
+
 export class Student extends Agent {
+    _state;
+    _desk;
+    _candies;
+
     constructor(p_app, p_classroom) {
         super(p_app, p_classroom);
+        this._state = StudentState.Idle;
+        this._candies = 0;
     }
     
     performAgentAction(action) {
         this.move(action);
+    }
+
+    choseAgentAction() {
+        this._classroom;
+
+        // Condition qui dit si le student veut un bonbon. Si il veut un bonbon, state devient MovingToCandy
+
+        // Si état = idle, return
+
+        // Sinon
+        // Si état = MovingToCandy, destination = bonbon le plus proche
+        // Si état = MovingToDesk, destination = son desk
+        // Calcule la route (pathfinding) pour aller à la destination
+        // Fait le prochain mouvement
+        // Si état = MovingToCandy, et si le student est sur une case adjacente au bonbon, state devient MovingToDesk et il a réussi à prendre le bonbon
+        // Si état = MovingToDesk, et si le student est sur son desk, state devient idle
+
     }
 
 }
--- src/Components/Teacher.jsx
+++ src/Components/Teacher.jsx
@@ -1,12 +1,46 @@
 import { Agent } from './Agent';
 
+export const TeacherState = {
+    Patrolling,
+    MovingToStudent
+}
+
 export class Teacher extends Agent {
+    _state;
+    _patrolPoints = [];
+    _patrolStatus = 0;
+
     constructor(p_app, p_classroom) {
         super(p_app, p_classroom);
+        this._state = TeacherState.Patrolling;
     }
     
     performAgentAction(action) {
         this.move(action);
+    }
+
+    choseAgentAction() {
+        this._classroom;
+
+        // Si il existe un student qui n'est pas à son bureau, state devient MovingToStudent
+
+        // Si état = Patrolling, destination = prochain point de patrouille
+        // Si état = MovingToStudent, destination = student le plus proche qui n'est pas à son bureau
+        // Calcule la route (pathfinding) pour aller à la destination
+        // Fait le prochain mouvement
+        // Si état = MovingToStudent, et si le teacher est sur une case adjacente au student, state devient Patrolling
+        // Si état = Patrolling, et si le teacher est sur un point de patrouille, il passe au prochain point de patrouille
+    }
+
+    // Retourne le prochain point de patrouille
+    // Si le teacher est sur le point i, retourne le point i+1
+    // Sinon le teacher va vert le point i
+    getPatrolPoint() {
+        if (this._patrolPoints.length === 0) return null;
+        if (this._gridPos.x === this._patrolPoints[this._patrolStatus].x && this._gridPos.y === this._patrolPoints[this._patrolStatus].y) {
+            this._patrolStatus = (this._patrolStatus + 1) % this._patrolPoints.length;
+        }
+        return this._patrolPoints[this._patrolStatus];
     }
 
 }
