8224f3e3b5d67593b3697ee2d33b000544202111
Vincent <flyber11@gmail.com>
2024-12-10 15:35:41 +0100

    css for menu is done

--- src/Components/MenuComponent.jsx
+++ src/Components/MenuComponent.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import MapDark from '../../src/assets/map_dark.png';
+import '../../src/styles/css/MenuComponent.css';
 
 const MenuComponent = ({
                            startGame,
@@ -13,54 +14,59 @@ const MenuComponent = ({
                            setStudentSpeed,
                            teacherSpeed,
                            setTeacherSpeed,
-
-                            studentCandyStrategy,
-                            setStudentCandyStrategy,
-                            teacherFocusStrategy,
-                            setTeacherFocusStrategy,
+                           studentCandyStrategy,
+                           setStudentCandyStrategy,
+                           teacherFocusStrategy,
+                           setTeacherFocusStrategy,
                        }) => {
-
 
     function clamp(value, min, max) {
         return Math.min(Math.max(value, min), max);
     }
 
     return (
-        // set background image and background size to cover and that div to take the whole screen and opacity of the background to 50%
-        <div id="menu" style={{backgroundImage: `url(${MapDark})`, width: '100vw', height: '100vh', backgroundSize: 'cover'}}>
+        <div id="menu" style={{ backgroundImage: `url(${MapDark})` }}>
             <h1>Menu</h1>
-            <h2>Number of candy : {sweetNumber}</h2>
-
-            <button onClick={() => setSweetNumber(clamp(sweetNumber - 1, 1, sweetNumber))}>Remove sweet jar</button>
-            <button onClick={() => setSweetNumber(sweetNumber + 1)}>Add sweet jar</button>
-            <h2>Number of students : {studentNumber}</h2>
-            <button onClick={() => setStudentNumber(clamp(studentNumber - 1, 1, studentNumber))}>Remove student
-            </button>
-            <button onClick={() => setStudentNumber(studentNumber + 1)}>Add student</button>
-            <h2>Number of teachers : {teacherNumber}</h2>
-            <button onClick={() => setTeacherNumber(clamp(teacherNumber - 1, 1, teacherNumber))}>Remove teacher
-            </button>
-            <button onClick={() => setTeacherNumber(teacherNumber + 1)}>Add teacher</button>
-            <h2>Student speed : {studentSpeed}</h2>
-            <button onClick={() => setStudentSpeed(clamp(studentSpeed - 1, 1, studentSpeed))}>Decrease speed</button>
-            <button onClick={() => setStudentSpeed(studentSpeed + 1)}>Increase speed</button>
-            <h2>Teacher speed : {teacherSpeed}</h2>
-            <button onClick={() => setTeacherSpeed(clamp(teacherSpeed - 1, 1, teacherSpeed))}>Decrease speed</button>
-            <button onClick={() => setTeacherSpeed(teacherSpeed + 1)}>Increase speed</button>
-            <h2>Student candy strategy</h2>
-            <select value={studentCandyStrategy} onChange={(e) => setStudentCandyStrategy(e.target.value)}>
-                <option value="Probability">Probability</option>
-                <option value="WhenTeacherIsFarBehind">When teacher is far behind</option>
-                <option value="WhenAnotherStudentStartsMoving">When another student starts moving</option>
-                <option value="Every5Seconds">Every 5 seconds</option>
-                <option value="Random">Random</option>
-            </select>
-            <h2>Teacher focus strategy</h2>
-            <select value={teacherFocusStrategy} onChange={(e) => setTeacherFocusStrategy(e.target.value)}>
-                <option value="ClosestStudent">Closest student</option>
-                <option value="ClosestStudentFocused">Closest student focused</option>
-            </select>
-            <button onClick={startGame}>Launch game</button>
+            <div id="menu-columns">
+                <div id="menu-column">
+                    <h2>Teacher Parameters</h2>
+                    <h2>Number of teachers: {teacherNumber}</h2>
+                    <button onClick={() => setTeacherNumber(clamp(teacherNumber - 1, 1, teacherNumber))}>Remove teacher</button>
+                    <button onClick={() => setTeacherNumber(teacherNumber + 1)}>Add teacher</button>
+                    <h2>Teacher speed: {teacherSpeed}</h2>
+                    <button onClick={() => setTeacherSpeed(clamp(teacherSpeed - 1, 1, teacherSpeed))}>Decrease speed</button>
+                    <button onClick={() => setTeacherSpeed(teacherSpeed + 1)}>Increase speed</button>
+                    <h2>Teacher focus strategy</h2>
+                    <select value={teacherFocusStrategy} onChange={(e) => setTeacherFocusStrategy(e.target.value)}>
+                        <option value="ClosestStudent">Closest Student</option>
+                        <option value="ClosestStudentFocused">Closest Student Focused</option>
+                    </select>
+                </div>
+                <div id="menu-column">
+                    <h2>Student Parameters</h2>
+                    <h2>Number of students: {studentNumber}</h2>
+                    <button onClick={() => setStudentNumber(clamp(studentNumber - 1, 1, studentNumber))}>Remove student</button>
+                    <button onClick={() => setStudentNumber(studentNumber + 1)}>Add student</button>
+                    <h2>Student speed: {studentSpeed}</h2>
+                    <button onClick={() => setStudentSpeed(clamp(studentSpeed - 1, 1, studentSpeed))}>Decrease speed</button>
+                    <button onClick={() => setStudentSpeed(studentSpeed + 1)}>Increase speed</button>
+                    <h2>Student candy strategy</h2>
+                    <select value={studentCandyStrategy} onChange={(e) => setStudentCandyStrategy(e.target.value)}>
+                        <option value="Probability">Probability</option>
+                        <option value="WhenTeacherIsFarBehind">When teacher is far behind</option>
+                        <option value="WhenAnotherStudentStartsMoving">When another student starts moving</option>
+                        <option value="Every5Seconds">Every 5 seconds</option>
+                        <option value="Random">Random</option>
+                    </select>
+                </div>
+                <div id="menu-column">
+                    <h2>Game Parameters</h2>
+                    <h2>Number of candy: {sweetNumber}</h2>
+                    <button onClick={() => setSweetNumber(clamp(sweetNumber - 1, 1, sweetNumber))}>Remove sweet jar</button>
+                    <button onClick={() => setSweetNumber(sweetNumber + 1)}>Add sweet jar</button>
+                    <button onClick={startGame} id="start-game">Start Game</button>
+                </div>
+            </div>
         </div>
     );
 };
