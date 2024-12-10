bf9e17b5d88a848a8948c0e2f02c6e148c173a52
Vincent <flyber11@gmail.com>
2024-12-10 13:54:15 +0100

    wip

--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -149,28 +149,15 @@ function displayClassroom(app, classroom) {
 
     // Charger et afficher les students
     for (let student of classroom._students) {
-        // Load the spritesheet JSON and image
-        PIXI.Assets.load('../../src/assets/student_spritesheet.json').then((spritesheetData) => {
-            const spritesheet = new PIXI.Spritesheet(
-                PIXI.Texture.from(spritesheetData.meta.image),
-                spritesheetData
-            );
-
-            // Parse the spritesheet
-            spritesheet.parse().then(() => {
-                // Create an AnimatedSprite using the frames from the spritesheet
-                const studentSprite = new PIXI.AnimatedSprite(spritesheet.animations['student_animation']);
-                studentSprite.zIndex = 11;
-                studentSprite.width = student._width;
-                studentSprite.height = student._height;
-                studentSprite.anchor.set(0.5, 1); // Set the anchor point to the center of the sprite to (1, 0.5) for each Agent's sprite to center it on the middle of the cell
-                studentSprite.animationSpeed = 0.1; // Adjust the animation speed as needed
-                studentSprite.play(); // Start the animation
-
-                app.stage.addChild(studentSprite);
-                student.setSprite(studentSprite);
-                student.display();
-            });
+        PIXI.Assets.load('../../src/assets/student.png').then((texture) => {
+            const studentSprite = new PIXI.Sprite(texture);
+            studentSprite.zIndex = 11;
+            studentSprite.width = student._width;
+            studentSprite.height = student._height;
+            studentSprite.anchor.set(0.5, 1); // Set the anchor point to the center of the sprite to (1, 0.5) for each Agent's sprite to center it on the middle of the cell
+            app.stage.addChild(studentSprite);
+            student.setSprite(studentSprite);
+            student.display();
         });
     }
 
