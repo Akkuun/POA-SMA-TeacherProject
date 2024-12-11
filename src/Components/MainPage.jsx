452764eb02209365d32650cadc2cfa0bf6a3c800
Mathis Duban <90316879+Akkuun@users.noreply.github.com>
2024-12-11 13:45:12 +0100

    Update MainPage.jsx

--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -237,7 +237,12 @@ const MainPage = ({sweetNumber, studentNumber, setSweetNumber, setStudentNumber,
 
     classroom.displayDesks(app);
     app.ticker.maxFPS = maxFPS;
+    let frames = 0;
     app.ticker.add(() => {
+        if (frames < 10) {
+            frames++;
+            return;
+        }
         if (classroom._state === "StartAnimation") {
             classroom.agentEnter();
             if (classroom._agentsWaitingToEnter.length === 0 && opened_door_sprite) {
